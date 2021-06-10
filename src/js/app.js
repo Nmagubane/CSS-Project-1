511a03e25043d4272e18586ecc0803bb27e1f677
Neo Magubane <neo.magubane@structureit.net>
2021-06-10 14:46:11 +0200

    first commit for project

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+
+function addNumbers() {
+    var number1 = parseInt(document.getElementById("number1").value);
+    var number2 = parseInt(document.getElementById("number2").value);
+
+    if (number1 > 100 || number1 < 1 && number2 > 100 || number2 < 1) {
+        document.getElementById("answer").value = "invalid input";
+    }
+    else {
+    document.getElementById("answer").value = number1 + number2;
+}
+}
