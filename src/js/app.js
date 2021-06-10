989255b3fdf5557588bd167c3e228089b1a0b182
Neo Magubane <neo.magubane@structureit.net>
2021-06-10 15:31:50 +0200

    updated app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,12 @@ function addNumbers() {
     var number1 = parseInt(document.getElementById("number1").value);
     var number2 = parseInt(document.getElementById("number2").value);
 
-    if (number1 > 100 || number1 < 1 && number2 > 100 || number2 < 1) {
-        document.getElementById("answer").value = document.getElementById("msg").innerText;
+    if (number1 > 100 || number1 < 1 || number2 > 100 || number2 < 1) {
+        document.getElementById("msg").style.display = "inline";
+        document.getElementById("answer").value = "Error"
     }
     else {
-    document.getElementById("answer").value = number1 + number2;
+        document.getElementById("msg").style.display = "none"
+        document.getElementById("answer").value = number1 + number2;
 }
 }
