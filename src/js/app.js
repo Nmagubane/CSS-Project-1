94c6429a39764efa153e8805e3931523c74d1fe1
Neo Magubane <neo.magubane@structureit.net>
2021-06-11 13:20:21 +0200

    updated html file and js file

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,50 @@ function addNumbers() {
         document.getElementById("answer").value = number1 + number2;
 }
 }
+
+function subtractNumbers() {
+    var number1 = parseInt(document.getElementById("number1").value);
+    var number2 = parseInt(document.getElementById("number2").value);
+
+    if (number1 > 100 || number1 < 1 || number2 > 100 || number2 < 1) {
+        document.getElementById("msg").style.display = "inline";
+        document.getElementById("answer").value = "Error"
+    }
+    else {
+        document.getElementById("msg").style.display = "none"
+        document.getElementById("answer").value = number1 - number2;
+}
+}
+
+function multiplyNumbers() {
+    var number1 = parseInt(document.getElementById("number1").value);
+    var number2 = parseInt(document.getElementById("number2").value);
+
+    if (number1 > 100 || number1 < 1 || number2 > 100 || number2 < 1) {
+        document.getElementById("msg").style.display = "inline";
+        document.getElementById("answer").value = "Error"
+    }
+    else {
+        document.getElementById("msg").style.display = "none"
+        document.getElementById("answer").value = number1 * number2;
+
+}
+}
+
+function divideNumbers() {
+    var number1 = parseInt(document.getElementById("number1").value);
+    var number2 = parseInt(document.getElementById("number2").value);
+
+    if (number1 > 100 || number1 < 1 || number2 > 100 || number2 < 1) {
+        document.getElementById("msg").style.display = "inline";
+        document.getElementById("answer").value = "Error"
+    }
+    else {
+        document.getElementById("msg").style.display = "none"
+        document.getElementById("answer").value = number1 / number2;
+}
+}
+
+function clearNumbers() {
+    document.getElementById("answer").value = "";
+}
